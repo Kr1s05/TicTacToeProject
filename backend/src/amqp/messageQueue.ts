fa9d00c9a7071543ab0bf7f8eec3e167a3bd6981
Kr1s05 <kristianoreshkov04@gmail.com>
2024-02-29 12:25:14 +0200

    connected bot to rabbitmq

--- backend/src/amqp/messageQueue.ts
+++ backend/src/amqp/messageQueue.ts
@@ -1,13 +1,19 @@
 import amqplib from "amqplib";
 import { env } from "process";
 
-const queue = "game";
+const queue = "game-queue";
+const exchange = "game";
 
 export async function setupMessaging() {
   const conn = await amqplib.connect(
     "amqp://" + env.MQUSER + ":" + env.MQPASS + "@rabbitmq"
   );
 
-  const ch1 = await conn.createChannel();
-  await ch1.assertQueue(queue);
+  const channel = await conn.createChannel();
+  await channel.assertQueue(queue, {durable:false});
+  await channel.assertExchange(exchange, 'direct', { durable: false });
+  await channel.bindQueue(queue, exchange, "game-queue")
+
+  channel.publish(exchange, 'game-queue', Buffer.from(JSON.stringify({Id:"adadadada",Board:["x","o","","",""],Turn:"x", Maximizing:"x"})));
 }
+
--- bot/src/Main.go
+++ bot/src/Main.go
@@ -4,12 +4,56 @@ import (
 	"os"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+
+	"encoding/json"
 )
 
 func main() {
-	connection, err := amqp.Dial("amqp://" + os.Getenv("MQUSER") + ":" + os.Getenv("MQPASS") + "@rabbitmq:5672/")
-	if err != nil {
-		panic(err)
-	}
+	connection, errCon := amqp.Dial("amqp://" + os.Getenv("MQUSER") + ":" + os.Getenv("MQPASS") + "@rabbitmq:5672/")
+	fail(errCon, "Connection error")
 	defer connection.Close()
+
+	channel, errCh := connection.Channel()
+	fail(errCh, "Channel error")
+
+	errEx := channel.ExchangeDeclare("game", "direct", false, false, false, false, nil)
+	fail(errEx, "Exchange error")
+
+	_, errQ := channel.QueueDeclare("game-queue", false, false, false, false, nil)
+	fail(errQ, "Queue error")
+
+	errB := channel.QueueBind("game-queue", "game-queue", "game", false, nil)
+	fail(errB, "BindError")
+
+	messages, errM := channel.Consume("game-queue", "game-consumer", false, false, false, false, nil)
+	fail(errM, "Consume error")
+
+	go func() {
+		for {
+			m := <-messages
+			var gs gameState
+			json.Unmarshal(m.Body, &gs)
+			println(gs.Board)
+			println(gs.Id)
+			m.Ack(false)
+		}
+	}()
+
+	ch1 := make(chan int)
+	<-ch1
+
+}
+
+func fail(err error, message string) {
+	if err != nil {
+		println(message)
+		panic(err.Error())
+	}
+}
+
+type gameState struct {
+	Id         string
+	Turn       string
+	Maximizing string
+	Board      []string
 }
